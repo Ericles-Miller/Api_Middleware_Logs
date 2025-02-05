8a831c87723e21dfe9b496978cc31b014fa9db84
Éricles Miller <ericlesmiller15@gmail.com>
2025-02-05 17:15:17 -0300

    feat: create new methods to find logs in database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/logs-db'),
-    LogsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-  exports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/logs-db'), LogsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
