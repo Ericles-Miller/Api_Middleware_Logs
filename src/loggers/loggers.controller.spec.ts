57c3c4082d25dc5d26b6a9dde7dd3565ffaeec3b
Éricles Miller <ericlesmiller15@gmail.com>
2025-02-06 20:01:55 -0300

    feat: create a new integration tests in controller

--- src/loggers/loggers.controller.spec.ts
+++ src/loggers/loggers.controller.spec.ts
@@ -1,50 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoggerController } from './logger.controller';
 import { LoggerService } from './logger.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Logger } from './entities/logger.entity';
 import { ELoggerLevel } from './logger-level.enum';
 
-describe('LogsController', () => {
+describe('LogsController (Integration Test)', () => {
   let controller: LoggerController;
   let service: LoggerService;
-  const loggers: Logger[] = [
-    {
-      id: '335ecab5-4e51-4bfe-9e3b-4dd115e7a47b',
-      method: 'GET',
-      url: 'http://localhost:3000/logs',
-      statusCode: 200,
-      ip: '127.0.0.1',
-      level: ELoggerLevel.INFO,
-      timeRequest: 100,
-      movieId: '',
-      timestamp: new Date(),
-    },
-  ];
-
-  const log: Logger = {
-    id: '335ecab5-4e51-4bfe-9e3b-4dd115e7a47b',
-    method: 'GET',
-    url: 'http://localhost:3000/logs',
-    statusCode: 200,
-    ip: '127.0.0.1',
-    level: ELoggerLevel.INFO,
-    timeRequest: 100,
-    movieId: '',
-    timestamp: new Date(),
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [LoggerController],
-      providers: [
-        {
-          provide: LoggerService,
-          useValue: {
-            getLogs: jest.fn().mockReturnValue(loggers),
-            getLogById: jest.fn().mockReturnValue(log),
-          },
-        },
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [Logger],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Logger]),
       ],
+      controllers: [LoggerController],
+      providers: [LoggerService],
     }).compile();
 
     controller = module.get<LoggerController>(LoggerController);
@@ -54,5 +31,46 @@ describe('LogsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('suit tests getLogs (Integration Test)', () => {
+    it('should be able to get all logs of database', async () => {
+      await service.logRequest('GET', 'http://localhost:3000/logs', 200, '127.0.0.1', ELoggerLevel.INFO, 100);
+      await service.logRequest('GET', 'http://localhost:3000/logs', 404, '127.0.0.1', ELoggerLevel.WARN, 100);
+
+      const result = await controller.getLogs();
+
+      expect(result.length).toBe(2);
+      expect(result[0].method).toBe('GET');
+      expect(result[0].url).toBe('http://localhost:3000/logs');
+      expect(result[0].statusCode).toBe(200);
+      expect(result[0].ip).toBe('127.0.0.1');
+      expect(result[0].level).toBe(ELoggerLevel.INFO);
+      expect(result[0].timeRequest).toBe(100);
+
+      expect(result[1].method).toBe('GET');
+      expect(result[1].url).toBe('http://localhost:3000/logs');
+      expect(result[1].statusCode).toBe(404);
+      expect(result[1].ip).toBe('127.0.0.1');
+      expect(result[1].level).toBe(ELoggerLevel.WARN);
+      expect(result[1].timeRequest).toBe(100);
+    });
+  });
+
+  describe('suit tests findLog (Integration Test)', () => {
+    it('should be able to find log by id', async () => {
+      await service.logRequest('GET', 'http://localhost:3000/logs', 200, '127.0.0.1', ELoggerLevel.INFO, 100);
+      const logs = await service.getLogs();
+
+      const result = await controller.getLogById(logs[0].id);
+
+      expect(result.id).toBe(logs[0].id);
+      expect(result.method).toBe(logs[0].method);
+      expect(result.url).toBe(logs[0].url);
+      expect(result.statusCode).toBe(logs[0].statusCode);
+      expect(result.ip).toBe(logs[0].ip);
+      expect(result.level).toBe(logs[0].level);
+      expect(result.timeRequest).toBe(logs[0].timeRequest);
+    });
   });
 });
